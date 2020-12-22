3e81fcfa4f17eee66e6a6ed2b523f969a59ab5ca
Ivan <ivan.ornes@gmail.com>
2020-12-22 23:07:06 +0100

    Added public access controls

--- FizzBuzz/FizzBuzz/FizzBuzz.swift
+++ FizzBuzz/FizzBuzz/FizzBuzz.swift
@@ -7,9 +7,8 @@
 
 import Foundation
 
-struct FizzBuzz {
-    
-    func process(number: Int) -> String {
+public struct FizzBuzz {
+    public func process(number: Int) -> String {
         return number.isMultiple(of: 3) ?
                (number.isMultiple(of: 5) ? "FizzBuzz": "Fizz") :
                 number.isMultiple(of: 5) ? "Buzz" : "\(number)"
