1ffd0179fbb762265a512f48b1dd86e78669fa36
Ivan <ivan.ornes@gmail.com>
2020-12-22 22:13:34 +0100

    Added the first test, given the number three, the result is Fizz

--- FizzBuzz/FizzBuzzTests/FizzBuzzTests.swift
+++ FizzBuzz/FizzBuzzTests/FizzBuzzTests.swift
@@ -8,6 +8,21 @@
 import XCTest
 @testable import FizzBuzz
 
-class FizzBuzzTests: XCTestCase {
+struct FizzBuzz {
+    func process(number: Int) -> String {
+        return "Fizz"
+    }
+}
 
+class FizzBuzzTests: XCTestCase {
+    func test_process_numberThreeResultIsFizz() {
+        // Given
+        let sut = FizzBuzz()
+        
+        // When
+        let result = sut.process(number: 3)
+        
+        // Then
+        XCTAssertEqual(result, "Fizz")
+    }
 }
